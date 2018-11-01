735c978f08f30bcad55c58e14df213a83fef9410
Web-Usov <web.usov@ya.ru>
2018-11-02 01:10:07 +0400

    set socket time-out

--- src/consts.js
+++ src/consts.js
@@ -12,6 +12,14 @@ export const SOCKET_TIME_OUT = 1000 * 20
 
 function time(){
     let date =  new Date();
-    date.setHours(date.getUTCHours()+4);
-    return date.getFullYear() + '-' +  date.getMonth() +'-' + date.getDay()+ ' '+ (date.getHours() < 10 ? '0' : '') + date.getHours() + ':' + date.getMinutes() + ':' + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+    var options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        timezone: 'UTC',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      }   
+    return date.toLocaleString("ru", options) 
 }
