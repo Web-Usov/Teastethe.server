17ef4d245d68f92544c05819a79d56d32bfac1f0
Web-Usov <web.usov@ya.ru>
2018-11-26 04:42:01 +0400

    fix#addTea#4

--- src/actions/teaActions.js
+++ src/actions/teaActions.js
@@ -1,11 +1,7 @@
 const Tea = require('../models/tea')
 
 const addTea = (props, cb) => {    
-    const {name,type} = props
-    Tea.create({
-        name,
-        type
-    },(error,docs) =>{
+    Tea.create(props,(error,docs) =>{
         if(error) cb(error.message)
         else cb(null,docs)
     })
